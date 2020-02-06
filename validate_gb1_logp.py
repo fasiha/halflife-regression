c8f0c4e9147fcc3698a01c2f94a293bda72675a5
Ahmed Fasih <wuzzyview@gmail.com>
2020-02-05 20:34:37 -0500

    use scipy logsumexp for logp demo

--- validate_gb1_logp.py
+++ validate_gb1_logp.py
@@ -1,3 +1,4 @@
+from scipy.special import logsumexp
 from scipy.stats import beta, binom
 from scipy.special import betaln
 import numpy as np
@@ -17,7 +18,7 @@ def genGb1Binom(Ndata, ALPHA, BETA, DELTA, N):
 ALPHA = 3.3
 BETA = 4.4
 DELTA = 3.33
-N = 5
+N = 15
 
 Ndata = 1_000_000
 obs = genGb1Binom(Ndata, ALPHA, BETA, DELTA, N)
@@ -27,14 +28,14 @@ def logp(k, alpha, beta, delta, n):
     i = np.arange(n - k + 1.0)
     mags = binomln(n - k, i) + betaln(delta * (i + k) + alpha, beta)
     signs = (-1.0)**i
-    return binomln(n, k) - betaln(alpha, beta) + logsumexp(mags, signs)
+    return binomln(n, k) - betaln(alpha, beta) + logsumexp(mags, b=signs)
 
 
-def logsumexp(x, signs):
-    "Adaptation of PyMC's logsumexp, but can take a list of signs like Scipy"
-    x_max = np.max(x)
-    result = np.sum(signs * np.exp(x - x_max))
-    return np.log(result if result >= 0 else -result) + x_max
+# def logsumexp(x, signs):
+#     "Adaptation of PyMC's logsumexp, but can take a list of signs like Scipy"
+#     x_max = np.max(x)
+#     result = np.sum(signs * np.exp(x - x_max))
+#     return np.log(result if result >= 0 else -result) + x_max
 
 
 def makeEmpiricalPmf(v):
@@ -46,6 +47,8 @@ def makeEmpiricalPmf(v):
 
 
 pmf = np.array([np.exp(logp(n, ALPHA, BETA, DELTA, N)) for n in range(N + 1)])
-assert np.allclose(np.sum(pmf), 1)
 pmfhat = np.array(makeEmpiricalPmf(obs))
+
+print(np.array([pmfhat, pmf]).T)
+assert np.allclose(np.sum(pmf), 1)
 assert np.all(np.abs(pmfhat - pmf) / np.abs(pmf) < .1)
