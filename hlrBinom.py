4279981f726cb669e9b2f77581c33b18ff8def04
Ahmed Fasih <wuzzyview@gmail.com>
2020-02-08 07:12:37 -0500

    LINTER and WHITESPACE ONLY: yapf

--- hlrBinom.py
+++ hlrBinom.py
@@ -4,31 +4,31 @@ import scipy.special as special
 
 
 def prob(k, n, t, x, w):
-    h = 2**(x @ w)
-    p = 2**(-t / h)
-    return stats.binom.pmf(k, n, p)
+  h = 2**(x @ w)
+  p = 2**(-t / h)
+  return stats.binom.pmf(k, n, p)
 
 
 def evaluate(w, df):
-    x = np.c_[df.sqrtright, df.sqrtwrong, np.ones(len(df))]
-    h = 2**(x @ w)
-    p = 2**(-df.t / h)
-    pmf = stats.binom.pmf(df.k, df.n, p)
+  x = np.c_[df.sqrtright, df.sqrtwrong, np.ones(len(df))]
+  h = 2**(x @ w)
+  p = 2**(-df.t / h)
+  pmf = stats.binom.pmf(df.k, df.n, p)
 
-    mae = np.mean(np.abs(p - df.obsp))
-    return dict(meanAbsoluteError=mae, meanPosterior=np.mean(pmf))
+  mae = np.mean(np.abs(p - df.obsp))
+  return dict(meanAbsoluteError=mae, meanPosterior=np.mean(pmf))
 
 
 def binomln(n, k):
-    # https://stackoverflow.com/a/21775412/500207
-    return -special.betaln(1 + n - k, 1 + k) - np.log(n + 1)
+  # https://stackoverflow.com/a/21775412/500207
+  return -special.betaln(1 + n - k, 1 + k) - np.log(n + 1)
 
 
 def probJacobian(k, n, t, x, w, widx):
-    h = 2**(x @ w)
-    p = 2**(-t / h)
-    pmf = stats.binom.pmf(k, n, p)
-    return pmf * x[widx] * np.log(2)**2 * t / (1 - p) * (k - n * p) / h
+  h = 2**(x @ w)
+  p = 2**(-t / h)
+  pmf = stats.binom.pmf(k, n, p)
+  return pmf * x[widx] * np.log(2)**2 * t / (1 - p) * (k - n * p) / h
 
 
 log2 = np.log(2)
@@ -36,50 +36,48 @@ loglog2Times2 = 2 * np.log(log2)
 
 
 def probJacobianAccurate(k, n, t, x, w, widx):
-    logh = x @ w * log2
-    h = np.exp(logh)
-    logp = -t / h * log2
-    log1MinusP = np.log(-np.expm1(logp))
-    logpmf = binomln(n, k) + k * logp + (n - k) * log1MinusP
-    logretbase = (logpmf + loglog2Times2 + np.log(t) - logh - log1MinusP +
-                  np.log(x[widx]))
-    return np.exp(logretbase) * k - np.exp(logretbase + logp + np.log(n))
+  logh = x @ w * log2
+  h = np.exp(logh)
+  logp = -t / h * log2
+  log1MinusP = np.log(-np.expm1(logp))
+  logpmf = binomln(n, k) + k * logp + (n - k) * log1MinusP
+  logretbase = (logpmf + loglog2Times2 + np.log(t) - logh - log1MinusP + np.log(x[widx]))
+  return np.exp(logretbase) * k - np.exp(logretbase + logp + np.log(n))
 
 
 def testAccuracy():
-    probJacobian(0, 1, 0.001597, np.array([16.822604, 12.041595, 1]),
-                 np.array([1.5386902, 1.60667677, 1.69713884]), 0)
-    probJacobianAccurate(0, 1, 0.001597, np.array([16.822604, 12.041595, 1]),
-                         np.array([1.5386902, 1.60667677, 1.69713884]), 0)
-    sumProbJac(0, 1, 0.001597, np.array([16.822604, 12.041595, 1]),
-               np.array([1.5386902, 1.60667677, 1.69713884]))
+  probJacobian(0, 1, 0.001597, np.array([16.822604, 12.041595, 1]),
+               np.array([1.5386902, 1.60667677, 1.69713884]), 0)
+  probJacobianAccurate(0, 1, 0.001597, np.array([16.822604, 12.041595, 1]),
+                       np.array([1.5386902, 1.60667677, 1.69713884]), 0)
+  sumProbJac(0, 1, 0.001597, np.array([16.822604, 12.041595, 1]),
+             np.array([1.5386902, 1.60667677, 1.69713884]))
 
 
 def sumProbJac(k, n, t, x, w):
-    h = 2**(x @ w)
-    p = 2**(-t / h)
-    pmf = stats.binom.pmf(k, n, p)
-    jacBase = pmf * np.log(2)**2 * t / (1 - p) * (k - n * p) / h
-    return (-np.sum(pmf), -(np.atleast_1d(jacBase) @ x))
+  h = 2**(x @ w)
+  p = 2**(-t / h)
+  pmf = stats.binom.pmf(k, n, p)
+  jacBase = pmf * np.log(2)**2 * t / (1 - p) * (k - n * p) / h
+  return (-np.sum(pmf), -(np.atleast_1d(jacBase) @ x))
 
 
 def mysumexp(a, b=1):
-    a_max = np.max(a)
-    s = np.sum(b * np.exp(a - a_max))
-    return s * np.exp(a_max)
+  a_max = np.max(a)
+  s = np.sum(b * np.exp(a - a_max))
+  return s * np.exp(a_max)
 
 
 def sumProbJacDf(w, x, df):
-    logh = (x @ w) * log2
-    h = np.exp(logh)
-    logp = (-df.t / h) * log2
-    log1MinusP = np.log(-np.expm1(logp))
-    logpmf = binomln(df.n, df.k) + df.k * logp + (df.n - df.k) * log1MinusP
+  logh = (x @ w) * log2
+  h = np.exp(logh)
+  logp = (-df.t / h) * log2
+  log1MinusP = np.log(-np.expm1(logp))
+  logpmf = binomln(df.n, df.k) + df.k * logp + (df.n - df.k) * log1MinusP
 
-    logretbase = logpmf + loglog2Times2 + np.log(df.t) - logh - log1MinusP
-    jacBase = np.exp(logretbase) * df.k - np.exp(logretbase + logp +
-                                                 np.log(df.n))
-    return (-mysumexp(logpmf), -(jacBase @ x))
+  logretbase = logpmf + loglog2Times2 + np.log(df.t) - logh - log1MinusP
+  jacBase = np.exp(logretbase) * df.k - np.exp(logretbase + logp + np.log(df.n))
+  return (-mysumexp(logpmf), -(jacBase @ x))
 
 
 import pandas as pd
@@ -88,8 +86,7 @@ fulldata['n'] = fulldata.session_seen
 fulldata['k'] = fulldata.session_correct
 fulldata['t'] = fulldata.delta / (60 * 60 * 24)  # convert time delta to days
 fulldata['sqrtright'] = np.sqrt(1 + fulldata.history_correct)
-fulldata['sqrtwrong'] = np.sqrt(1 + (fulldata.history_seen -
-                                     fulldata.history_correct))
+fulldata['sqrtwrong'] = np.sqrt(1 + (fulldata.history_seen - fulldata.history_correct))
 fulldata[
     'obsp'] = fulldata.session_correct / fulldata.session_seen  # clip these to compare to HLR paper?
 
@@ -108,30 +105,27 @@ def adaGrad(weights,
             minibatchsize=250,
             verbose=True,
             verboseIteration=100):
-    weights = weights.copy()
-    gti = np.zeros_like(weights)
+  weights = weights.copy()
+  gti = np.zeros_like(weights)
 
-    for t in range(max_it):
-        df = df.sample(frac=1.0)
-        x = x[df.index, :]
-        xslice = slice(0, minibatchsize)
-        while xslice.start < len(df):
-            # https://stackoverflow.com/a/34879805/500207
-            sd = df[xslice]
-            sx = x[xslice, :]
-            val, grad = sumProbJacDf(weights, sx, sd)
-            gti += grad**2
-            adjusted_grad = grad / (fudge_factor + np.sqrt(gti))
-            weights -= stepsize * adjusted_grad
-            if verbose and (xslice.start % verboseIteration == 0):
-                # prob = objective(weights, df)
-                print(
-                    "# Iteration {}/{:_}, weights={}, |grad|^2={:.1e}, Δ={:.1e}"
-                    .format(t, xslice.start, weights, np.sum(grad**2),
-                            np.sqrt(np.sum(adjusted_grad**2))))
-            xslice = slice(xslice.start + minibatchsize,
-                           xslice.stop + minibatchsize, xslice.step)
-    return weights
+  for t in range(max_it):
+    df = df.sample(frac=1.0)
+    x = x[df.index, :]
+    xslice = slice(0, minibatchsize)
+    while xslice.start < len(df):
+      # https://stackoverflow.com/a/34879805/500207
+      sd = df[xslice]
+      sx = x[xslice, :]
+      val, grad = sumProbJacDf(weights, sx, sd)
+      gti += grad**2
+      adjusted_grad = grad / (fudge_factor + np.sqrt(gti))
+      weights -= stepsize * adjusted_grad
+      if verbose and (xslice.start % verboseIteration == 0):
+        # prob = objective(weights, df)
+        print("# Iteration {}/{:_}, weights={}, |grad|^2={:.1e}, Δ={:.1e}".format(
+            t, xslice.start, weights, np.sum(grad**2), np.sqrt(np.sum(adjusted_grad**2))))
+      xslice = slice(xslice.start + minibatchsize, xslice.stop + minibatchsize, xslice.step)
+  return weights
 
 
 # np.seterr(all='raise')
@@ -142,33 +136,30 @@ X = np.c_[data.sqrtright, data.sqrtwrong, np.ones(len(data))]
 
 
 def testJac():
-    import numdifftools as nd
-    k = 2
-    n = 3
-    t = 1.1
-    x = np.array([1, 2, 3.])
-    w1 = -.1
-    w2 = -.3
-    w0 = -.2
-    print([
-        nd.Derivative(lambda w: prob(k, n, t, x, np.array([w1, w, w0])))(w2),
-        probJacobian(k, n, t, x, np.array([w1, w2, w0]), 1)
-    ])
+  import numdifftools as nd
+  k = 2
+  n = 3
+  t = 1.1
+  x = np.array([1, 2, 3.])
+  w1 = -.1
+  w2 = -.3
+  w0 = -.2
+  print([
+      nd.Derivative(lambda w: prob(k, n, t, x, np.array([w1, w, w0])))(w2),
+      probJacobian(k, n, t, x, np.array([w1, w2, w0]), 1)
+  ])
 
-    def obj(k, n, t, x, w):
-        h = 2**(x @ w)
-        p = 2**(-t / h)
-        pmf = stats.binom.pmf(k, n, p)
-        return -np.sum(pmf)
+  def obj(k, n, t, x, w):
+    h = 2**(x @ w)
+    p = 2**(-t / h)
+    pmf = stats.binom.pmf(k, n, p)
+    return -np.sum(pmf)
 
-    X = np.c_[data.sqrtright, data.sqrtwrong, np.ones(len(data))]
-    [[
-        nd.Derivative(lambda w: obj(data.k, data.n, data.t, X,
-                                    np.array([w, .01, 4.4])))(-.3),
-        nd.Derivative(lambda w: obj(data.k, data.n, data.t, X,
-                                    np.array([-.3, w, 4.4])))(.01),
-        nd.Derivative(lambda w: obj(data.k, data.n, data.t, X,
-                                    np.array([-.3, .01, w])))(4.4)
-    ],
-     sumProbJac(data.k, data.n, data.t, X, np.array([-.3, .01, 4.4])),
-     sumProbJacDf([-.3, .01, 4.4], X, data)]
+  X = np.c_[data.sqrtright, data.sqrtwrong, np.ones(len(data))]
+  [[
+      nd.Derivative(lambda w: obj(data.k, data.n, data.t, X, np.array([w, .01, 4.4])))(-.3),
+      nd.Derivative(lambda w: obj(data.k, data.n, data.t, X, np.array([-.3, w, 4.4])))(.01),
+      nd.Derivative(lambda w: obj(data.k, data.n, data.t, X, np.array([-.3, .01, w])))(4.4)
+  ],
+   sumProbJac(data.k, data.n, data.t, X, np.array([-.3, .01, 4.4])),
+   sumProbJacDf([-.3, .01, 4.4], X, data)]
--- logitFixedK.py
+++ logitFixedK.py
@@ -1,123 +1,111 @@
-import scipy.optimize as opt
 from scipy.special import betaln
 import numpy as np
 import pandas as pd
-from scipy.special import logsumexp, psi, logit, expit
-import numdifftools as nd
-from functools import lru_cache
+from scipy.special import logsumexp, psi, expit
 
 
 def binomln(n, k):
-    # https://stackoverflow.com/a/21775412/500207
-    return -betaln(1 + n - k, 1 + k) - np.log(n + 1)
+  # https://stackoverflow.com/a/21775412/500207
+  return -betaln(1 + n - k, 1 + k) - np.log(n + 1)
 
 
-def optimized(k, alpha, beta, delta, n, mu, kappa, logitMu, logitKappa,
-              featureVecMu, featureVecKappa):
-    i = np.arange(n - k + 1.0)
-    ds = delta * (i + k)
-    tosum = np.exp(
-        binomln(n, k) + binomln(n - k, i) + betaln(ds + alpha, beta) -
-        betaln(alpha, beta))
-    tosum *= (-1.0)**i
+def optimized(k, alpha, beta, delta, n, mu, kappa, logitMu, logitKappa, featureVecMu,
+              featureVecKappa):
+  i = np.arange(n - k + 1.0)
+  ds = delta * (i + k)
+  tosum = np.exp(binomln(n, k) + binomln(n - k, i) + betaln(ds + alpha, beta) - betaln(alpha, beta))
+  tosum *= (-1.0)**i
 
-    prob = np.sum(tosum)
+  prob = np.sum(tosum)
 
-    kappaPrime = kappa / (np.exp(logitKappa) + 1)
-    muPrime = mu / (np.exp(logitMu) + 1)
+  kappaPrime = kappa / (np.exp(logitKappa) + 1)
+  muPrime = mu / (np.exp(logitMu) + 1)
 
-    psiMu = psi(ds + alpha) - psi(alpha)
-    baseJacMu = np.sum(psiMu * tosum) * muPrime / kappa
+  psiMu = psi(ds + alpha) - psi(alpha)
+  baseJacMu = np.sum(psiMu * tosum) * muPrime / kappa
 
-    psiKappa = -mu * psiMu + psi(ds + 1 / kappa) - psi(1 / kappa)
-    baseJacKappa = np.sum(psiKappa * tosum) * kappaPrime / kappa**2
-    if not np.isfinite(baseJacKappa):
-        import pdb
-        pdb.set_trace()
-    return (prob,
-            np.hstack(
-                [baseJacMu * featureVecMu, baseJacKappa * featureVecKappa]))
+  psiKappa = -mu * psiMu + psi(ds + 1 / kappa) - psi(1 / kappa)
+  baseJacKappa = np.sum(psiKappa * tosum) * kappaPrime / kappa**2
+  if not np.isfinite(baseJacKappa):
+    import pdb
+    pdb.set_trace()
+  return (prob, np.hstack([baseJacMu * featureVecMu, baseJacKappa * featureVecKappa]))
 
 
 def optimizedObjective(weights, df):
-    wm1, wm2, wm0, wk0 = weights
-    logitMus = df.feature1 * wm1 + df.feature2 * wm2 + wm0
-    mus = expit(logitMus)
-    logitKappas = np.ones(len(df)) * wk0
-    kappas = expit(logitKappas)
-    alphas = mus / kappas
-    betas = (1 - mus) / kappas
-    deltas = df.delta_.values
-    ns = df.n.values
-    ks = df.k.values
+  wm1, wm2, wm0, wk0 = weights
+  logitMus = df.feature1 * wm1 + df.feature2 * wm2 + wm0
+  mus = expit(logitMus)
+  logitKappas = np.ones(len(df)) * wk0
+  kappas = expit(logitKappas)
+  alphas = mus / kappas
+  betas = (1 - mus) / kappas
+  deltas = df.delta_.values
+  ns = df.n.values
+  ks = df.k.values
 
-    totalY = 0
-    totalJac = 0
-    for (i, args) in enumerate(
-            zip(ks, alphas, betas, deltas, ns, mus, kappas, logitMus,
-                logitKappas)):
-        featureVec = np.array([df.feature1.iloc[i], df.feature2.iloc[i], 1.0])
-        y, jac = optimized(*args, featureVec, featureVec[-1:])
-        totalY += y
-        totalJac += jac
-    return (-totalY, -totalJac)
+  totalY = 0
+  totalJac = 0
+  for (i,
+       args) in enumerate(zip(ks, alphas, betas, deltas, ns, mus, kappas, logitMus, logitKappas)):
+    featureVec = np.array([df.feature1.iloc[i], df.feature2.iloc[i], 1.0])
+    y, jac = optimized(*args, featureVec, featureVec[-1:])
+    totalY += y
+    totalJac += jac
+  return (-totalY, -totalJac)
 
 
 def logp(k, alpha, beta, delta, n):
-    i = np.arange(n - k + 1.0)
-    mags = binomln(n - k, i) + betaln(delta * (i + k) + alpha, beta)
-    signs = (-1.0)**i
-    # if delta is 0.000005, logsumexp could be 0 (log0 = 1) or negative, causing underflow.
-    # solutions: sqrt(delta), or clip here
-    return binomln(n, k) - betaln(alpha, beta) + logsumexp(
-        mags, b=signs, return_sign=False)
+  i = np.arange(n - k + 1.0)
+  mags = binomln(n - k, i) + betaln(delta * (i + k) + alpha, beta)
+  signs = (-1.0)**i
+  # if delta is 0.000005, logsumexp could be 0 (log0 = 1) or negative, causing underflow.
+  # solutions: sqrt(delta), or clip here
+  return binomln(n, k) - betaln(alpha, beta) + logsumexp(mags, b=signs, return_sign=False)
 
 
 def pclip(p):
-    # bound min/max model predictions (helps with loss optimization)
-    return np.clip(p, a_min=0.0001, a_max=.99999)
+  # bound min/max model predictions (helps with loss optimization)
+  return np.clip(p, a_min=0.0001, a_max=.99999)
 
 
 def evaluate(weights, df):
-    wm1, wm2, wm0, wk0 = weights
-    logitMus = df.feature1 * wm1 + df.feature2 * wm2 + wm0
-    mus = expit(logitMus)
-    logitKappas = np.ones(len(df)) * wk0
-    kappas = expit(logitKappas)
-    alphas = mus / kappas
-    betas = (1 - mus) / kappas
-    deltas = df.delta_.values
-    ns = df.n.values
-    ks = df.k.values
+  wm1, wm2, wm0, wk0 = weights
+  logitMus = df.feature1 * wm1 + df.feature2 * wm2 + wm0
+  mus = expit(logitMus)
+  logitKappas = np.ones(len(df)) * wk0
+  kappas = expit(logitKappas)
+  alphas = mus / kappas
+  betas = (1 - mus) / kappas
+  deltas = df.delta_.values
+  ns = df.n.values
+  ks = df.k.values
 
-    priorProbability = np.exp(
-        betaln(alphas + deltas, betas) - betaln(alphas, betas))
-    observedProbability = pclip(ks / ns)
+  priorProbability = np.exp(betaln(alphas + deltas, betas) - betaln(alphas, betas))
+  observedProbability = pclip(ks / ns)
 
-    mae = np.mean(np.abs(priorProbability - observedProbability))
+  mae = np.mean(np.abs(priorProbability - observedProbability))
 
-    # posteriors = np.exp(np.vectorize(logp)(ks, alphas, betas, deltas, ns))
-    # meanPosterior = np.mean(posteriors)
-    # quantiles = [0.1, 0.5, 0.9]
-    # quantileValues = []  # np.quantile(posteriors, quantiles)
+  # posteriors = np.exp(np.vectorize(logp)(ks, alphas, betas, deltas, ns))
+  # meanPosterior = np.mean(posteriors)
+  # quantiles = [0.1, 0.5, 0.9]
+  # quantileValues = []  # np.quantile(posteriors, quantiles)
 
-    return dict(meanAbsoluteError=mae)
+  return dict(meanAbsoluteError=mae)
 
 
 def count2feature(x, log=True):
-    return np.log(1 + x) + 1 if log else np.sqrt(1 + x)
+  return np.log(1 + x) + 1 if log else np.sqrt(1 + x)
 
 
 fulldata = pd.read_csv("features.csv")  # features.csv for full
 fulldata['feature1'] = count2feature(fulldata.history_correct)
-fulldata['feature2'] = count2feature(fulldata.history_seen -
-                                     fulldata.history_correct)
+fulldata['feature2'] = count2feature(fulldata.history_seen - fulldata.history_correct)
 fulldata['delta_'] = np.sqrt(fulldata.delta / (3600 * 24))
 fulldata['n'] = fulldata.session_seen
 fulldata['k'] = fulldata.session_correct
 fulldata['sqrtright'] = np.sqrt(1 + fulldata.history_correct)
-fulldata['sqrtwrong'] = np.sqrt(1 + (fulldata.history_seen -
-                                     fulldata.history_correct))
+fulldata['sqrtwrong'] = np.sqrt(1 + (fulldata.history_seen - fulldata.history_correct))
 
 
 # https://github.com/benbo/adagrad/blob/master/adagrad.py
@@ -129,29 +117,26 @@ def adaGrad(weights,
             minibatchsize=250,
             verbose=True,
             verboseIteration=1000):
-    weights = weights.copy()
-    gti = np.zeros_like(weights)
+  weights = weights.copy()
+  gti = np.zeros_like(weights)
 
-    for t in range(max_it):
-        df = df.sample(frac=1.0)
-        xslice = slice(0, minibatchsize)
-        while xslice.start < len(df):
-            sd = df.sample(minibatchsize).reset_index(
-                drop=True)  # https://stackoverflow.com/a/34879805/500207
-            val, grad = optimizedObjective(weights, sd)
-            gti += grad**2
-            adjusted_grad = grad / (fudge_factor + np.sqrt(gti))
-            weights -= stepsize * adjusted_grad
-            if verbose and (xslice.start % verboseIteration) == 0:
-                # prob = objective(weights, df)
-                print(
-                    "# Iteration {}/{:_}, weights={}, |grad|^2={:.1e}, Δ={:.1e}"
-                    .format(t, xslice.start, weights, np.sum(grad**2),
-                            np.sqrt(np.sum(adjusted_grad**2))))
-            xslice = slice(xslice.start + minibatchsize,
-                           xslice.stop + minibatchsize, xslice.step)
+  for t in range(max_it):
+    df = df.sample(frac=1.0)
+    xslice = slice(0, minibatchsize)
+    while xslice.start < len(df):
+      sd = df.sample(minibatchsize).reset_index(
+          drop=True)  # https://stackoverflow.com/a/34879805/500207
+      val, grad = optimizedObjective(weights, sd)
+      gti += grad**2
+      adjusted_grad = grad / (fudge_factor + np.sqrt(gti))
+      weights -= stepsize * adjusted_grad
+      if verbose and (xslice.start % verboseIteration) == 0:
+        # prob = objective(weights, df)
+        print("# Iteration {}/{:_}, weights={}, |grad|^2={:.1e}, Δ={:.1e}".format(
+            t, xslice.start, weights, np.sum(grad**2), np.sqrt(np.sum(adjusted_grad**2))))
+      xslice = slice(xslice.start + minibatchsize, xslice.stop + minibatchsize, xslice.step)
 
-    return weights
+  return weights
 
 
 Ndata = round(len(fulldata) * 0.9)
